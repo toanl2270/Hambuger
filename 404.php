b82c6a438d392b314eb2cd7b2fa78d1689e73cee
toanl2270 <toanl2270@gmail.com>
2022-07-15 20:52:17 +0900

    0715

--- 404.php
+++ 404.php
@@ -1,6 +1,6 @@
 <?php get_header(); ?>
    <div id="content" class="narrowcolumn">
-     <h2 class="center">Error 404 - Not Found!!（ページがありません。）</h2>
+     <h2 class="center">Error 404 - Not Found!!（ページがありません！）</h2>
 	</div>
 <?php get_sidebar(); ?>
 <?php get_footer(); ?>
